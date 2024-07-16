cb1b418d0678553e08d53cf7a13f0333ce980606
ldjimenezt <ldjimenezt@utn.edu.ec>
2024-07-16 17:32:47 -0500

    Fix image visibility on mobile and update background image

--- src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
+++ src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function BackgroundRadialLeft() {
     return (
-        <div className="left-0 absolute -z-50 top-0 md:hidden">
+        <div className="left-0 absolute -z-50 top-0 hidden md:block">
             <Image src="/assets/background-floated-right.png" width={850} height={650} alt="Circle Left" />
         </div>
     )
--- src/components/BackgroundRadialRight/BackgroundRadialRight.tsx
+++ src/components/BackgroundRadialRight/BackgroundRadialRight.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function BackgroundRadialRight() {
     return (
-        <div className="right-0 absolute -z-50 top-0">
+        <div className="right-0 absolute -z-50 top-0 hidden md:block">
             <Image src="/assets/background-floated-right.png" width={1000} height={1000} alt="Circle Right" />
         </div>
     )
